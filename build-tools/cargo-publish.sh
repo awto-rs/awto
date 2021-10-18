7cd6a2129683c0cccc5df936a851b955015aa569
Ari Seyhun <ariseyhun@live.com.au>
2021-10-18 11:59:05 +0700

    chore: create script for cargo publish

---
+++ build-tools/cargo-publish.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+echo "awto-macros"
+cd awto-macros
+sed -i '' -e 's/^version.*$/version = "'$1'"/' Cargo.toml
+git commit -am "chore(awto-macros): version $1"
+cargo publish
+cd ..
+sleep 10
+
+echo "awto"
+cd awto
+sed -i '' -e 's/^version.*$/'"version = \"$1\"/" Cargo.toml
+sed -i '' -e 's/^awto-macros [^,]*,/awto-macros = { version = "'$1'",/' Cargo.toml
+git commit -am "chore(awto): version $1"
+cargo publish
+cd ..
+sleep 10
+
+echo "awto-compile"
+cd awto-compile
+sed -i '' -e 's/^version.*$/'"version = \"$1\"/" Cargo.toml
+sed -i '' -e 's/^awto [^,]*,/awto = { version = "'$1'",/' Cargo.toml
+git commit -am "chore(awto-compile): version $1"
+cargo publish
+cd ..
+sleep 10
+
+echo "awto-macros"
+cd awto-macros
+sed -i '' -e 's/^version.*$/version = "'$1'"/' Cargo.toml
+git commit -am "chore(awto-macros): version $1"
+cargo publish
+cd ..
+sleep 10
